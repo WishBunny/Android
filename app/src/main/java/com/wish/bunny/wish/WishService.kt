e10f28b2bfbb69253fdd157ee441e1b914b65577
codeSweet0828 <codesweet0828@gmail.com>
2024-01-23 17:07:40 +0900

    위시리스트 상세 ing

--- app/src/main/java/com/wish/bunny/wish/WishService.kt
+++ app/src/main/java/com/wish/bunny/wish/WishService.kt
@@ -5,6 +5,7 @@ import com.wish.bunny.wish.domain.Wish
 import com.wish.bunny.wish.domain.WishItem
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface WishService {
@@ -12,4 +13,8 @@ interface WishService {
     // fun getWishList(): Call<List<>>
     @GET("wish/list")
     fun getWishList(): Call<List<WishItem>>
+
+    @GET("wish/{wishNo}")
+    fun getWishDetail(@Path("wishNo") wishNo: String): Call<WishItem>
+
 }
