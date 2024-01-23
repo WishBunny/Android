390eedc5f552e90c5d824abe47fc1ddad3b2ca21
tori <sangeun99@khu.ac.kr>
2024-01-23 15:25:38 +0900

    Feat: 안드로이드에서 카카오 인가코드 받은 것을 바로 서버로 요청 날리기 - 로그인 완료 시 Shared Preferences에 저장할 수 있도록 설정 #10

---
+++ app/src/main/java/com/wish/bunny/sharedpreferences/PreferenceUtil.kt
@@ -0,0 +1,22 @@
+package com.wish.bunny.sharedpreferences
+
+import android.content.Context
+import android.content.SharedPreferences
+
+/**
+    작성자: 엄상은
+    처리 내용: Shared Preferences 객체 초기화
+*/
+class PreferenceUtil(context: Context) {
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences("prefs_name", Context.MODE_PRIVATE)
+
+    fun getString(key: String, defValue: String): String {
+        // defValue는 값이 없을 경우 대체되어 나올 값
+        return prefs.getString(key, defValue).toString()
+    }
+
+    fun setString(key: String, str: String) {
+        prefs.edit().putString(key, str).apply()
+    }
+}
