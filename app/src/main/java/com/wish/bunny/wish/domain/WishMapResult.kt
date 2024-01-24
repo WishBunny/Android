dc07c40f62644237f397d8bf714467cdaa2bcb06
codeSweet0828 <codesweet0828@gmail.com>
2024-01-24 19:13:39 +0900

    WishMapResult Commit & PUSH

---
+++ app/src/main/java/com/wish/bunny/wish/domain/WishMapResult.kt
@@ -0,0 +1,9 @@
+package com.wish.bunny.wish.domain
+
+import com.wish.bunny.wish.domain.WishItem
+import java.util.Objects
+
+data class WishMapResult(
+    val result: Objects,
+    val list : List<WishItem>
+)
