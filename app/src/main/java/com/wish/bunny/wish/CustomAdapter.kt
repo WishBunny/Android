c760ba3a18ed372263fc94d59c7b7ece94b3ad26
codeSweet0828 <codesweet0828@gmail.com>
2024-01-25 02:14:51 +0900

    버킷리스트 완료

--- app/src/main/java/com/wish/bunny/wish/CustomAdapter.kt
+++ app/src/main/java/com/wish/bunny/wish/CustomAdapter.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import android.text.SpannableString
 import android.text.style.ClickableSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,11 +14,17 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.wish.bunny.R
+import com.wish.bunny.retrofit.RetrofitConnection
 import com.wish.bunny.wish.domain.WishItem
+import com.wish.bunny.wish.domain.WishMapResult
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
-
+import android.app.AlertDialog
+import android.content.DialogInterface
 class CustomAdapter(private val context: Context, private val wishItemList: List<WishItem>) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
@@ -58,10 +65,49 @@ class CustomAdapter(private val context: Context, private val wishItemList: List
                     onDetailButtonClickListener?.onDetailButtonClick(wishItemList[position].wishNo)
                 }
             }
-            //버튼클릭시
             itemView.findViewById<Button>(R.id.rv_detail_btn).setOnClickListener {
-                
+                showConfirmationDialog(wishItemList[position].wishNo)
             }
+        }
+
+        //버킷리스트 완료 확인 메세지
+        private fun showConfirmationDialog(wishNo: String) {
+            val context = itemView.context
+            val alertDialogBuilder = AlertDialog.Builder(context)
+            alertDialogBuilder.setTitle("버킷리스트 완료")
+            alertDialogBuilder.setMessage("정말로 해당 버킷리스트를 완료처리 하시겠습니까?")
+            alertDialogBuilder.setPositiveButton("Yes") { dialogInterface, _ ->
+                doneWishDetail(wishNo)
+                dialogInterface.dismiss()
+            }
+            alertDialogBuilder.setNegativeButton("No") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
+
+
+        private fun doneWishDetail(wishNo : String) {
+            val retrofitAPI = RetrofitConnection.getInstance().create(WishService::class.java)
+            retrofitAPI.finishWish(wishNo).enqueue(object : Callback<WishMapResult> {
+                override fun onResponse(call: Call<WishMapResult>, response: Response<WishMapResult>) {
+                    val wishMapResult = response.body()
+
+                    if (wishMapResult != null) {
+                        // updateUI(wishMapResult.list)
+                        Log.d("doneWishDetail", wishMapResult.result.toString())
+                    } else {
+                        Log.d("doneWishDetail", "서버 응답이 null입니다.")
+                    }
+                }
+
+                override fun onFailure(call: Call<WishMapResult>, t: Throwable) {
+                    Log.d("doneWishDetail", "불러오기 실패: ${t.message}")
+
+                }
+            })
         }
 
         fun bind(wishItem: WishItem) {
