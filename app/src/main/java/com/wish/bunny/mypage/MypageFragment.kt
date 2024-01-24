8bef5e47ba0c6dab513d2532da0df76abe89a0c8
tori <sangeun99@khu.ac.kr>
2024-01-24 10:08:48 +0900

    Feat: 카카오 SDK 연결 끊기 처 #10

--- app/src/main/java/com/wish/bunny/mypage/MypageFragment.kt
+++ app/src/main/java/com/wish/bunny/mypage/MypageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kakao.sdk.user.UserApiClient
 import com.wish.bunny.GlobalApplication
 import com.wish.bunny.databinding.FragmentMypageBinding
 
@@ -26,7 +27,27 @@ class MypageFragment : Fragment() {
         binding.LogoutBtn.setOnClickListener {
             logout()
         }
+        binding.DisconnectBtn.setOnClickListener {
+            disconnect()
+        }
         return binding.root
+    }
+
+    /**
+        작성자: 엄상은
+        처리 내용: 카카오 연결 끊기 처리
+    */
+    private fun disconnect() {
+        Log.d("MyPageFragement", "회원탈퇴 시도")
+        GlobalApplication.prefs.removeKeyWithValue("accessToken")
+        UserApiClient.instance.unlink { error ->
+            if (error != null) {
+                Log.e("MyPageFragement", "연결 끊기 실패", error)
+            }
+            else {
+                Log.i("MyPageFragement", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
+            }
+        }
     }
 
     /**
