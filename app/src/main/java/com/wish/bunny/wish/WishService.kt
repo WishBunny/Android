359a9735a483fe2dc1821ee692a2783072d1d0cb
tori <sangeun99@khu.ac.kr>
2024-01-25 12:10:01 +0900

    Fix: conflict 해결

--- app/src/main/java/com/wish/bunny/wish/WishService.kt
+++ app/src/main/java/com/wish/bunny/wish/WishService.kt
@@ -14,17 +14,14 @@ import retrofit2.http.Path
 
 
 interface WishService {
-
-    // fun getWishList(): Call<List<>>
-    @GET("wish/list")
-    fun getWishList(): Call<List<WishItem>>
-
     /**
     작성자: 황수연
     처리 내용: 위시 데이터 등록 서비스
      */
     @POST("wish")
     fun wishInsert(@Body wvo: WishVo): Call<Response<Message>>
+
+    @GET("wish/list")
     fun getWishList(): Call<WishMapResult>
 
     @GET("wish/{wishNo}")
