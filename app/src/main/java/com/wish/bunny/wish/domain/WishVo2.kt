4f711aa2f9746a375a25c4b70b660c11e60502cd
roonhyeon <roonhyeon@gmail.com>
2024-01-28 13:16:36 +0900

    엄데이트 오류 수정

--- app/src/main/java/com/wish/bunny/wish/domain/WishVo2.kt
+++ app/src/main/java/com/wish/bunny/wish/domain/WishVo2.kt
@@ -5,10 +5,12 @@ package com.wish.bunny.wish.domain
 처리 내용: 위시 데이터 수정 VO
  */
 data class WishVo2(
+    val wishNo: String? = null,
     val content: String,
     val deadlineDt: String,
     val category: String,
     val notifyYn: String,
+    val achieveYn: String,
     val tagNo: String,
     val deleteTag: String
 )
