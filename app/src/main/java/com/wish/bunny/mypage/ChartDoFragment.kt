711650a4d739944ee1e60f3d2e78792ecb95ca1f
hyeyeon <tjsalasu@naver.com>
2024-01-29 09:12:13 +0900

    feat: 차트 색상 및 legend변경

--- app/src/main/java/com/wish/bunny/mypage/ChartDoFragment.kt
+++ app/src/main/java/com/wish/bunny/mypage/ChartDoFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
@@ -37,12 +38,8 @@ class ChartDoFragment(private val percentDo:Float, private val percentEat:Float,
             entries.add(PieEntry(achieve, "달성 위시🐰"))
 
             val colorsItems = ArrayList<Int>()
-            for (c in ColorTemplate.VORDIPLOM_COLORS) colorsItems.add(c)
-            for (c in ColorTemplate.JOYFUL_COLORS) colorsItems.add(c)
-            for (c in COLORFUL_COLORS) colorsItems.add(c)
-            for (c in ColorTemplate.LIBERTY_COLORS) colorsItems.add(c)
-            for (c in ColorTemplate.PASTEL_COLORS) colorsItems.add(c)
-            colorsItems.add(ColorTemplate.getHoloBlue())
+            colorsItems.add(Color.parseColor("#DADADA"))
+            colorsItems.add(Color.parseColor("#FFE1E3"))
 
             val pieDataSet = PieDataSet(entries, "")
             pieDataSet.apply {
@@ -59,6 +56,15 @@ class ChartDoFragment(private val percentDo:Float, private val percentEat:Float,
                 centerText = message
                 setEntryLabelColor(Color.BLACK)
                 setCenterTextSize(15f)
+
+                legend.apply {
+                    horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+                    verticalAlignment = Legend.LegendVerticalAlignment.CENTER
+                    orientation = Legend.LegendOrientation.VERTICAL
+                    setDrawInside(false)
+                    isEnabled = true
+                }
+
                 animateY(1400, Easing.EaseInOutQuad)
                 animate()
             }
--- app/src/main/java/com/wish/bunny/mypage/ChartGetFragment.kt
+++ app/src/main/java/com/wish/bunny/mypage/ChartGetFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
@@ -35,12 +36,8 @@ class ChartGetFragment(private val percentDo:Float, private val percentEat:Float
             entries.add(PieEntry(100-achieve, "🥕"))
             entries.add(PieEntry(achieve, "달성 위시🐰"))
             val colorsItems = ArrayList<Int>()
-            for (c in ColorTemplate.VORDIPLOM_COLORS) colorsItems.add(c)
-            for (c in ColorTemplate.JOYFUL_COLORS) colorsItems.add(c)
-            for (c in ColorTemplate.COLORFUL_COLORS) colorsItems.add(c)
-            for (c in ColorTemplate.LIBERTY_COLORS) colorsItems.add(c)
-            for (c in ColorTemplate.PASTEL_COLORS) colorsItems.add(c)
-            colorsItems.add(ColorTemplate.getHoloBlue())
+            colorsItems.add(Color.parseColor("#DADADA"))
+            colorsItems.add(Color.parseColor("#FFE1E3"))
 
             val pieDataSet = PieDataSet(entries, "")
             pieDataSet.apply {
@@ -56,7 +53,16 @@ class ChartGetFragment(private val percentDo:Float, private val percentEat:Float
                 isRotationEnabled = false
                 centerText = message
                 setEntryLabelColor(Color.BLACK)
-                setCenterTextSize(14f)
+                setCenterTextSize(15f)
+
+                legend.apply {
+                    horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+                    verticalAlignment = Legend.LegendVerticalAlignment.CENTER
+                    orientation = Legend.LegendOrientation.VERTICAL
+                    setDrawInside(false)
+                    isEnabled = true
+                }
+
                 animateY(1400, Easing.EaseInOutQuad)
                 animate()
             }
