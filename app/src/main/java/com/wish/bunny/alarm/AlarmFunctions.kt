e7cdc13cd892816d61c798a2ac1afe8234de4569
hyeyeon <tjsalasu@naver.com>
2024-01-30 16:49:24 +0900

    refactor: 친구 및 마이페이지 리팩토링

--- app/src/main/java/com/wish/bunny/alarm/AlarmFunctions.kt
+++ app/src/main/java/com/wish/bunny/alarm/AlarmFunctions.kt
@@ -5,6 +5,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Calendar
